9667c470ef5bcf8f498b6750e277543b9cf0c36e
Jacob Wilson <jacob-841@hotmail.com>
2019-11-20 15:31:32 -0600

    Touched up the templates to store username and email.

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, render_template
+import jinja2
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/")
+def index():
+    return render_template("signup.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    email = request.form["email"]
+    username_error = ""
+    password1_error = ""
+    password2_error = ""
+    email_error = ""
+    if len(username) < 3 or len(username) > 20:
+        username_error = "Username must be 3-20 characters long"
+    else:
+        if username.find(" ") >= 1:
+            username_error = "Username must not include spaces"
+    if len(password1) < 3 or len(password1) > 20:
+        password1_error = "Password must be 3-20 characters long"
+    else:
+        if password1.find(" ") >= 1:
+            password1_error = "Password must not include spaces"
+    if password2 != password1:
+        password2_error = "Passwords must match"
+    if email != "":
+        if len(email) < 3 or len(email) > 20:
+            email_error = "Email must be 3-20 characters long"
+        else:
+            if email.find(" ") >= 1:
+                email_error = "Email must not include spaces"
+            elif email.find(".") == 0 and email.find("@") == 0:
+                email_error = "Email must include at least 1 @ and ."
+            elif email.find(".") == 0:
+                email_error = "Email must include at least 1 ."
+            elif email.find("@") == 0:
+                email_error = "Email must include at least 1 @"
+    if not username_error and not password1_error and not password2_error and not email_error:
+        return render_template("login.html", username=username)
+    else:
+        return render_template("signup.html", username_error=username_error,password1_error=password1_error,password2_error=password2_error,email_error=email_error,username=username,email=email)
+app.run()
